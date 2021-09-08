3c0b588df54112b64ebce8d3dfc5cefb3a4d57ef
Anton Kachurin <anton.kachurin@t-systems.com>
2021-09-08 17:28:15 +0300

    Store test fixtures in temporary directory (#6)
    Copy `.tmpl` files to temporary dir
    
    Allows avoiding accidental project pollution with fixtures

--- tests/lint_test.go
+++ tests/lint_test.go
@@ -2,10 +2,13 @@ package tests
 
 import (
 	"fmt"
+	"io/fs"
 	"io/ioutil"
+	"log"
 	"math/rand"
 	"os"
 	"path/filepath"
+	"strings"
 	"sync"
 	"testing"
 	"time"
@@ -16,6 +19,7 @@ import (
 )
 
 var cwd string
+var tmpDir string
 
 func init() {
 	rand.Seed(time.Now().UnixNano())
@@ -27,8 +31,59 @@ func init() {
 	cwd = wd
 }
 
+func copyFile(src, dest string) error {
+	b, err := ioutil.ReadFile(src)
+	if err != nil {
+		return err
+	}
+	return ioutil.WriteFile(dest, b, 0600)
+}
+
+func copyDir(src, dest string) error {
+	return filepath.Walk(src, func(p string, i fs.FileInfo, err error) error {
+		if i.IsDir() {
+			return nil
+		}
+		rDest, err := filepath.Rel(src, p)
+		if err != nil {
+			return err
+		}
+		if !strings.HasSuffix(rDest, ".tmpl") {
+			return nil
+		}
+		rDest = strings.Replace(rDest, ".tmpl", "", 1)
+		destFile := filepath.Join(dest, rDest)
+		if err := os.MkdirAll(filepath.Dir(destFile), 0700); err != nil {
+			return err
+		}
+		return copyFile(p, destFile)
+	})
+}
+
+func TestMain(m *testing.M) {
+	var err error
+	tmpDir, err = os.MkdirTemp("", "tsl-test-*")
+	if err != nil {
+		log.Fatalf("can't create temp dir for test fixtures: %s", err)
+	}
+	log.Printf("temporary test dir created at %s", tmpDir)
+
+	// copy test fixture files:
+	localFixtures := fmt.Sprintf("%s/fixtures", cwd)
+	err = copyDir(localFixtures, tmpDir)
+	if err != nil {
+		log.Fatalf("error copying fixtures to temp dir: %s", err)
+	}
+
+	r := m.Run()
+	if err := os.RemoveAll(tmpDir); err != nil {
+		_, _ = fmt.Fprintf(os.Stderr, err.Error())
+	}
+	os.Exit(r)
+}
+
 func fixturePath(name string) string {
-	return fmt.Sprintf("%s/fixtures/%s", cwd, name)
+	return filepath.Join(tmpDir, name)
 }
 
 func TestListingResourceGenerators(t *testing.T) {
